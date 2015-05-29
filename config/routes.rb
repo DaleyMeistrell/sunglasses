94597d35c5b168ea6f34a7a326cc1a95588dff02
Daley Meistrell <dstrell14@gmail.com>
2015-05-28 20:49:46 -0700

    sign up authentication working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get "products" => "products#index"
   get "/products/:name" => "products#show", as: :product
+  get "users" => "users#index"
+  get "users/new" => "users#new"
+  post "users" => "users#create"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
